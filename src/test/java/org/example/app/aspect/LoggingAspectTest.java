c5d51ceb8fa69a9660beef5e06b44599ab435428
PetrosiunArtem <petrosiun.av@phystech.edu>
2025-03-07 20:43:23 +0300

    update LoggingAspectTest

--- src/test/java/org/example/app/aspect/LoggingAspectTest.java
+++ src/test/java/org/example/app/aspect/LoggingAspectTest.java
@@ -1,14 +1,18 @@
 package org.example.app.aspect;
 
+import org.example.app.AppApplication;
 import org.example.app.controller.FilesController;
+import org.example.app.security.SecurityConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 import java.net.MalformedURLException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@ContextConfiguration(classes = {AppApplication.class, SecurityConfig.class})
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class LoggingAspectTest {
 
